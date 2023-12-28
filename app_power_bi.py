df81afb143235e90065dc502d373508fc8424b2f
AtrattivaTecnologia <155094740+AtrattivaTecnologia@users.noreply.github.com>
2023-12-28 16:23:27 -0300

    Update app_power_bi.py

--- app_power_bi.py
+++ app_power_bi.py
@@ -5,12 +5,14 @@ st.set_page_config(page_title="Portifolio")
 
 
 with st.container():
-    st.subheader("Meu primeiro site com o streamlit")
-    st.title("Dashboard")
-    st.write("Informações sobre as eleições")
-    st.write("Dashboard Eleições [Clique aqui](https://app.powerbi.com/reportEmbed?reportId=27816bbc-2039-47f7-bf96-44f0d09ea2a6&autoAuth=true&ctid=0d5d7d7a-b007-411f-9929-2091d2189c64)")
+    st.subheader("Portifólio")
+    st.title("Dashboard - Marcio")
+    
     
 with st.container():
     st.write("---")
-    
-    
+    st.write("Informações sobre as eleições")
+    st.write("Dashboard Eleições [Clique aqui](https://app.powerbi.com/reportEmbed?reportId=27816bbc-2039-47f7-bf96-44f0d09ea2a6&autoAuth=true&ctid=0d5d7d7a-b007-411f-9929-2091d2189c64)")
+
+with st.container():
+    st.write("---")
